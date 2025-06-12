772a30d952a04df32440bd43df38375c6c7bd1ea
Crystyan Verly <crystyanknappverly@gmail.com>
2025-06-11 22:44:46 -0300

    build js and css  solves

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
+// menu ativo
+
 const nav_menu = document.querySelectorAll(".header-menu a");
 const link_pagina = window.location.href;
-const btn_compra_bike = document.querySelector(".buy-container .btn");
-console.log(btn_compra_bike);
 
 nav_menu.forEach((e) => {
   const link_elemento = e.href;
@@ -9,4 +9,57 @@ nav_menu.forEach((e) => {
   if (link_pagina.includes(link_elemento)) {
     e.classList.add("menu-ativo");
   }
+});
+
+// ativar itens do orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+parametros.forEach(ativarProduto);
+
+// accordion
+
+const accordion_btn = document.querySelectorAll(".question-container button");
+
+function toggleAccordion(e) {
+  const pergunta = e.target;
+  const ariaControl = pergunta.getAttribute("aria-controls");
+  const resposta = document.querySelector(`#${ariaControl}`);
+  const isExpanded = pergunta.getAttribute("aria-expanded");
+
+  pergunta.setAttribute("aria-expanded", String(isExpanded !== "true"));
+  resposta.classList.toggle("accordion-ativo");
+}
+
+accordion_btn.forEach((btn) => {
+  btn.addEventListener("click", toggleAccordion);
+});
+
+// galeria de imagem
+
+const galeria = document.querySelectorAll(".bikes-galery-container img");
+
+function primeiroNaFila(e) {
+  if (matchMedia("(min-width: 800px)").matches)
+    e.target.parentElement.prepend(e.target);
+}
+
+function scaleImage(e) {
+  e.target.style.transform = "scale(1.025)";
+}
+function resetScale(e) {
+  e.target.style.transform = "scale(1)";
+}
+
+galeria.forEach((img) => {
+  img.style.transition = "transform 0.4s ease";
+  img.addEventListener("click", primeiroNaFila);
+  img.addEventListener("mouseover", scaleImage);
+  img.addEventListener("mouseout", resetScale);
 });
